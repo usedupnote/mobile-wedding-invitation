75e9c4075894a6f494542db86dca78f29d76d3b1
usedupnote <leejm9704@naver.com>
2024-09-27 20:34:18 +0900

    update: 실습 내용 진행

--- src/firebase.ts
+++ src/firebase.ts
@@ -3,8 +3,16 @@ import { getDatabase } from 'firebase/database';
 
 // TODO: https://firebase.google.com/docs/web/setup?hl=ko
 // 가이드에 따라 firebase config를 설정합니다.
-const firebaseConfig = {
 
+const firebaseConfig = {
+  apiKey: "AIzaSyDLn2xYBxkEmCdjBvOnPwspBHdTKv30IS4",
+  authDomain: "mobile-invitation-96894.firebaseapp.com",
+  databaseURL: "https://mobile-invitation-96894-default-rtdb.firebaseio.com",
+  projectId: "mobile-invitation-96894",
+  storageBucket: "mobile-invitation-96894.appspot.com",
+  messagingSenderId: "903089448813",
+  appId: "1:903089448813:web:fc35a580005ddc17135c64",
+  measurementId: "G-W5JRYR2B1L"
 };
 
 export const firebaseApp = initializeApp(firebaseConfig);
